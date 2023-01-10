65dbff914d0b367877bd998526db92b60082c55f
lansmancai <119608107+lansmancai@users.noreply.github.com>
2023-01-10 19:16:49 +0800

    Create BurpExtender.java

---
+++ httpdrop/BurpExtender.java
@@ -0,0 +1,29 @@
+package burp;
+
+import java.io.PrintWriter;
+
+public class BurpExtender implements IBurpExtender, IProxyListener {
+
+    private PrintWriter stdout;
+
+    @Override
+    public void registerExtenderCallbacks(final IBurpExtenderCallbacks callbacks) {
+        callbacks.setExtensionName("Her0in");
+        this.stdout = new PrintWriter(callbacks.getStdout(), true);
+        callbacks.registerProxyListener(this);
+    }
+
+    @Override
+    public void processProxyMessage(boolean messageIsRequest,
+                                    IInterceptedProxyMessage message) {
+        // 只操作请求消息
+        if (messageIsRequest) {
+            // 获取并打印出客户端 IP
+            this.stdout.println(message.getClientIpAddress());
+			this.stdout.println("what happen?");
+            // Drop 掉所有请求
+            message.setInterceptAction(IInterceptedProxyMessage.ACTION_DROP);
+            // TODO here
+        }
+    }
+}
